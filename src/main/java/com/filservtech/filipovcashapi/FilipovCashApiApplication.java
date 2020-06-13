b888a634c9888f1a5a697b523b6b773ceda82075
Marcelo Filipov <marcelo.filipov@gmail.com>
2020-06-13 09:09:44 -0300

    The First Commit

---
+++ src/main/java/com/filservtech/filipovcashapi/FilipovCashApiApplication.java
@@ -0,0 +1,13 @@
+package com.filservtech.filipovcashapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilipovCashApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilipovCashApiApplication.class, args);
+	}
+
+}
