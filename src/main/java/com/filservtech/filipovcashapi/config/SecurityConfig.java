44bdcc81cf6f191f6e512f185bfd86874582c8ce
Marcelo Filipov <marcelo.filipov@gmail.com>
2020-06-23 21:48:33 -0300

    Security implemented

--- src/main/java/com/filservtech/filipovcashapi/config/SecurityConfig.java
+++ src/main/java/com/filservtech/filipovcashapi/config/SecurityConfig.java
@@ -1,4 +1,31 @@
 package com.filservtech.filipovcashapi.config;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ROLE");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                    .antMatchers("/categorias").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .httpBasic().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .csrf().disable();
+    }
+
 }
