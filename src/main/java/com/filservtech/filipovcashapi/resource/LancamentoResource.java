065ae4e23500c813f42a56b8bb600e82be1f5c8f
Marcelo Filipov <marcelo.filipov@gmail.com>
2020-07-02 20:58:33 -0300

    Implementing lançamento projection

--- src/main/java/com/filservtech/filipovcashapi/resource/LancamentoResource.java
+++ src/main/java/com/filservtech/filipovcashapi/resource/LancamentoResource.java
@@ -5,6 +5,7 @@ import com.filservtech.filipovcashapi.exceptionhandler.CashExceptionHandler;
 import com.filservtech.filipovcashapi.model.Lancamento;
 import com.filservtech.filipovcashapi.repository.LancamentoRepository;
 import com.filservtech.filipovcashapi.repository.filter.LancamentoFilter;
+import com.filservtech.filipovcashapi.repository.projection.ResumoLancamento;
 import com.filservtech.filipovcashapi.service.LancamentoService;
 import com.filservtech.filipovcashapi.service.exception.PessoaInexistenteOuInativaException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +44,12 @@ public class LancamentoResource {
     @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
     public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
         return lancamentoRepository.filtrar(lancamentoFilter, pageable);
+    }
+
+    @GetMapping(params = "resumo")
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
+    public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
+        return lancamentoRepository.resumir(lancamentoFilter, pageable);
     }
 
     @GetMapping("/{codigo}")
