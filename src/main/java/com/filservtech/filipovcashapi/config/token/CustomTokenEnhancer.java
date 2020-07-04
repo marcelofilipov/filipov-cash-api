3a258390a4812c7e0567b352d88bf78331bec73a
Marcelo Filipov <marcelo.filipov@gmail.com>
2020-07-04 09:56:24 -0300

    Username in JWT Token implemented

--- src/main/java/com/filservtech/filipovcashapi/config/token/CustomTokenEnhancer.java
+++ src/main/java/com/filservtech/filipovcashapi/config/token/CustomTokenEnhancer.java
@@ -1,4 +1,25 @@
 package com.filservtech.filipovcashapi.config.token;
 
-public class CustomTokenEnhancer {
+import com.filservtech.filipovcashapi.security.UserSystem;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomTokenEnhancer implements TokenEnhancer {
+
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+        UserSystem userSystem = (UserSystem) authentication.getPrincipal();
+
+        Map<String, Object> addInfo = new HashMap<>();
+        addInfo.put("nome", userSystem.getUsuario().getNome());
+
+        ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(addInfo);
+
+        return accessToken;
+    }
 }
--- src/main/java/com/filservtech/filipovcashapi/security/UserSystem.java
+++ src/main/java/com/filservtech/filipovcashapi/security/UserSystem.java
@@ -1,4 +1,24 @@
 package com.filservtech.filipovcashapi.security;
 
-public class UserSystem {
+import com.filservtech.filipovcashapi.model.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class UserSystem extends User {
+
+    private static final long serialVersionUID = 1L;
+
+    private Usuario usuario;
+
+    public UserSystem(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+        super(usuario.getEmail(), usuario.getSenha(), authorities);
+        this.usuario = usuario;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
 }
